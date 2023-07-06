4fbaece3372e9220592e4e032e2cdd589e061073
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2023-07-06 17:36:36 -0500

    fix: retry failed openai requests

--- conlang_gpt/openai.py
+++ conlang_gpt/openai.py
@@ -1,5 +1,7 @@
 import os
+import time
 
+import click
 import dotenv
 import openai
 
@@ -14,5 +16,24 @@ openai.api_key = os.environ["OPENAI_API_KEY"]
 def complete_chat(**kwargs):
     """Complete a chat with the OpenAI API."""
 
-    return openai.ChatCompletion.create(**kwargs)
+    delay = 1
+    while True:
+        try:
+            completion = openai.ChatCompletion.create(**kwargs)
+            break
+        except openai.error.APIError as e:
+            click.echo(click.style(f"OpenAI API error: {e}. Retrying in {delay} second(s)...", fg="red"))
+            time.sleep(delay)
+        except openai.error.RateLimitError:
+            click.echo(click.style(f"OpenAI API rate limit exceeded. Retrying in {delay} second(s)...", fg="red"))
+            time.sleep(delay)
+            delay *= 2
+        except openai.error.ServiceUnavailableError:
+            click.echo(click.style(f"OpenAI API service unavailable. Retrying in {delay} second(s)...", fg="red"))
+            time.sleep(delay)
+            delay *= 2
+        except openai.error.Timeout:
+            click.echo(click.style(f"OpenAI API timeout. Retrying in {delay} second(s)...", fg="red"))
+            time.sleep(delay)
 
+    return completion
