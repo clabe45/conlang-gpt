c3da5ef2b9e733fb6b71c0b14e93d9263b1bd869
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2023-07-08 16:02:38 -0500

    fix: increase presence penalty when creating guide
    Decreases likelihood of the conlang drawing too much inspiration from
    English

--- conlang_gpt/language.py
+++ conlang_gpt/language.py
@@ -156,6 +156,7 @@ def generate_language(design_goals, model):
     chat_completion = complete_chat(
         model=model,
         temperature=0.9,
+        presence_penalty=0.5,
         messages=[
             {"role": "user", "content": f"Create a constructed language with the following design goals:\n\n{design_goals}\n\nYour response should be an exhaustive reference sheet including all the information that is needed to use the language. Assume the reader has no prior experience using the language."}
         ],
