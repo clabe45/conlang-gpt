ad74246cedb21a17267faa6d90eb8d64d0fa18ac
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2023-07-08 09:29:03 -0500

    fix: fix incorrect usage of the word 'rows'

--- conlang_gpt/language.py
+++ conlang_gpt/language.py
@@ -228,7 +228,7 @@ def create_dictionary(guide, mode, count, model, embeddings_model) -> dict:
             model=model,
             temperature=0.9,
             messages=[
-                {"role": "user", "content": f"Generate {count} single random vocabulary words related to '{topic}' for the following constructed language. Format your response as a CSV document with two rows: Word and English Translation.\n\nLanguage guide:\n\n{guide}"}
+                {"role": "user", "content": f"Generate {count} single random vocabulary words related to '{topic}' for the following constructed language. Format your response as a CSV document with two columns: Word and English Translation.\n\nLanguage guide:\n\n{guide}"}
             ]
         )
         response = chat_completion['choices'][0]['message']['content']
